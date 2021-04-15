2edb7e177546fd507d07f3cced183054bd18e247
savannazanco <savannazanco@gmail.com>
2021-04-15 14:54:31 +0000

    added color

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,7 +36,7 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight, 100, groundY, 'blue');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'blue');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
